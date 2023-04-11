7ade9dbb0629475ecd4fd27bd75ef66fbbfa6a6f
matthewruns1 <130129395+matthewruns1@users.noreply.github.com>
2023-04-11 13:59:03 -0700

    Update js-demo.js

--- js-demo.js
+++ js-demo.js
@@ -7,19 +7,19 @@
       Returns the text of the day of the week where dayNum
       is the number of the week from 0 (Sunday) to 6 (Saturday)
 */
-
 runClock();
 setInterval("runClock()" , 1000);
 
 function runClock(){
-   var thisDay=new Date();
-   var thisDate=thisDay.toLocaleDateString();
-   var thisDayNum=thisDay.getDay();
-   var thisWeekday=getWeekday(thisDayNum);
-   var thisTime=thisDay.toLocaleTimeString();
-   document.getElementsById("date").textContent=thisDate;
-   document.getElementById("wday").textContent=thisWeekday;
-   document.getElementById("time").textContent=thisTime;
+ var thisDay= new Date();
+ var thisDate=thisDay.toLocaleDateString();
+ var thisDayNum=thisDay.getDay();
+  var thisWeekday=getWeekday(thisDayNum);
+  var thisTime=thisDay.toLocaleTimeString();
+ 
+ document.getElementById("date").textContent=thisDate;
+ document.getElementById("wday").textContent=thisWeekday;
+ document.getElementById("time").textContent=thisTime;
 }
 
 function getWeekday(dayNum){
